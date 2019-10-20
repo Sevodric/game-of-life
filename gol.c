ce383b933d49475dbd24ac2e13f58b6015dbf7dd
Rémi <remi.durieu92@gmail.com>
2019-10-20 19:41:11 +0200

    Réécrit gol.c -> jeu partiellement fonctionnel

--- gol.c
+++ gol.c
@@ -1,14 +1,25 @@
+//  À FAIRE :
+//    - Perfectionner la fonction board_update pour prendre en compte les bords
+//      du plateau
+//    - Ajouter des fonctions d'initialisation de configurations de départ
+//    - Afficher la génération actuelle et le nombre de cellules vivantes dans
+//      la barre de titre
+//    - Définir une structure contenant les informations utiles (n-ième
+//      génération, nombre de cellules vivantes, etc)
+//  ----------------------------------------------------------------------------
+
 // gol.c : Le jeu de la vie, selon les règles de John Conway :
 //    "À chaque étape, l’évolution d’une cellule est entièrement déterminée par
 //    l’état de ses huit voisines de la façon suivante :
 //
-//    - Une cellule morte possédant exactement trois voisines vivantes devient 
+//    - Une cellule morte possédant exactement trois voisines vivantes devient
 //        vivante (elle naît).
-//    - Une cellule vivante possédant deux ou trois voisines vivantes le reste, 
-//        sinon elle meurt." 
+//    - Une cellule vivante possédant deux ou trois voisines vivantes le reste,
+//        sinon elle meurt."
 //    (https://fr.wikipedia.org/wiki/Jeu_de_la_vie)
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 #include "sg.h"
 
@@ -22,25 +33,31 @@
 #define FGCOLOR WHITE
 #define TITLE "Game of Life"
 
-#define NCELL 100   // Nombre de cellules par lignes/colonnes
-#define SCELL 10    // Taille d'une cellule en pixels
+// Taille du plateau
+#define BOARD_SIZE 50
 
-// Une cellule est définie par son coin supérieur gauche et son état binaire
-//    0 = morte, 1 = vivante
+// Taile d'une cellule en pixels
+#define CELL_SIZE 20
+
 typedef struct {
-  double x;
-  double y;
-  int state;
-} cell;
+  int current_gen[BOARD_SIZE][BOARD_SIZE];
+  int next_gen[BOARD_SIZE][BOARD_SIZE];
+  int gen;
+} board;
 
-// board_init : initialise le plateau avec des cellules mortes
-void board_init(cell board[][NCELL]);
+//  board_init : initialise le plateau pointé par ptr avec des celulles mortes
+void board_init(board *ptr);
 
-// board_draw : dessine le plateau
-void board_draw(cell board[][NCELL]);
+//  board_update : met à jour le plateau pointé par ptr selon les règles
+//    précisées précedemment
+void board_update(board *ptr);
 
-// board_update : 
-void board_update(cell board[][NCELL]);
+//  board_draw : dessine le plateau pointé par ptr
+void board_draw(board *ptr);
+
+//  alive_neighbors : renvoie le nombre de voisins vivants de la cellule de
+//    coordonnées x, y du plateau pointé par ptr
+int alive_neighbors(board *ptr, int x, int y);
 
 // --- FONCTION PRINCIPALE
 // -----------------------------------------------------------------------------
@@ -48,15 +65,20 @@ void board_update(cell board[][NCELL]);
 int main(void) {
   // Initialisation
   srand((unsigned int) time(NULL));
-  cell board[NCELL][NCELL];
-  board_init(board);
-  board[10][9].state = 1;
-  board[10][10].state = 1;
-  board[10][11].state = 1;
   sg_open(WIDTH, HEIGHT, BGCOLOR, FGCOLOR, TITLE);
+  board b;
+  board_init(&b);
+  b.current_gen[10][9] = 1;
+  b.current_gen[9][10] = 1;
+  b.current_gen[10][10] = 1;
+  b.current_gen[11][10] = 1;
+  b.current_gen[9][11] = 1;
+  b.current_gen[11][11] = 1;
+  b.current_gen[10][12] = 1;
   while (sg_get_key() != 'q') {
-    board_draw(board);
-    board_update(board);
+    board_draw(&b);
+    board_update(&b);
+    printf("Génération %d\n", b.gen);
   }
   sg_close();
   return EXIT_SUCCESS;
@@ -65,69 +87,53 @@ int main(void) {
 // --- DEVELOPPEMMENTS
 // -----------------------------------------------------------------------------
 
-// --- board_init
-void board_init(cell board[][NCELL]) {
-  double x = 12;
-  double y = 12;
-  for (size_t i = 0; i < NCELL; i++) {
-    for (size_t j = 0; j < NCELL; j++) {
-      board[i][j] = (cell) {
-        x, y, 0
-      };
-      x += SCELL;
+void board_init(board *ptr) {
+  for (int x = 0; x < BOARD_SIZE; ++x) {
+    for (int y = 0; y < BOARD_SIZE; ++y) {
+      ptr->current_gen[x][y] = 0;
     }
-    x = 12;
-    y += SCELL;
   }
+  ptr->gen = 0;
 }
 
-// --- board_draw
-void board_draw(cell board[][NCELL]) {
-  for (int i = 0; i < NCELL; i++) {
-    for (int j = 0; j < NCELL; j++) {
-      if (board[i][j].state == 1) {
-        sg_fill_rectangle((int) board[i][j].x, (int) board[i][j].y, SCELL,
-            SCELL);
+void board_update(board *ptr) {
+  for (int x = 0; x < BOARD_SIZE; ++x) {
+    for (int y = 0; y < BOARD_SIZE; ++y) {
+      if (alive_neighbors(ptr, x, y) == 2 || alive_neighbors(ptr, x, y) == 3) {
+        ptr->next_gen[x][y] = 1;
+      } else {
+        ptr->next_gen[x][y] = 0;
       }
     }
   }
+  for (int x = 0; x < BOARD_SIZE; ++x) {
+    for (int y = 0; y < BOARD_SIZE; ++y) {
+      ptr->current_gen[x][y] = ptr->next_gen[x][y];
+    }
+  }
+  ptr->gen += 1;
 }
 
-// --- board_update
-void board_update(cell board[][NCELL]) {
-  for (int i = 0; i < NCELL; i++) {
-    for (int j = 0; j < NCELL; j++) {
-      int count = 0;
-      if (board[j - 1][i - 1].state == 1) {
-        count++;
-      }
-      if (board[j - 1][i].state == 1) {
-        count++;
-      }
-      if (board[j - 1][i + 1].state == 1) {
-        count++;
-      }
-      if (board[j][i + 1].state == 1) {
-        count++;
-      }
-      if (board[j + 1][i + 1].state == 1) {
-        count++;
-      }
-      if (board[j + 1][i].state == 1) {
-        count++;
-      }
-      if (board[j + 1][i - 1].state == 1) {
-        count++;
-      }
-      if (board[j][i - 1].state == 1) {
-        count++;
-      }
-      if (board[j][i].state == 0 && count == 3) {
-        board[j][i].state = 1;
-      } else if (board[j][i].state == 1 && !(count == 3 || count == 2)) {
-        board[j][i].state = 0;
+void board_draw(board *ptr) {
+  sg_clear();
+  for (int x = 0; x < BOARD_SIZE; ++x) {
+    for (int y = 0; y < BOARD_SIZE; ++y) {
+       if (ptr->current_gen[x][y] == 0){
+        sg_draw_rectangle(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+      } else if (ptr->current_gen[x][y] == 1) {
+        sg_fill_rectangle(x * CELL_SIZE, y * CELL_SIZE, CELL_SIZE, CELL_SIZE);
       }
     }
   }
 }
 
+int alive_neighbors(board *ptr, int x, int y) {
+  int n = 0;
+  for (int i = x - 1; i <= x + 1; ++i) {
+    for (int j = y - 1; j <= y + 1; ++j) {
+      n += ptr->current_gen[i][j];
+    }
+  }
+  n -= 1;
+  return n;
+}
